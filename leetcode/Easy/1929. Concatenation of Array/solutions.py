0ea7a0bb30bba570d5e9e8a2e88b006182fa183a
AJackTi <dtrong97vn@gmail.com>
2023-04-05 21:58:31 +0700

    feat: 1929. Concatenation of Array problems

---
+++ leetcode/Easy/1929. Concatenation of Array/solutions.py
@@ -0,0 +1,10 @@
+def getConcatenation(nums: list[int]) -> list[int]:
+    output = []
+    for i in range(len(nums)*2):
+        output.append(i%len(nums))
+    
+    return output
+
+nums = [1,2,1]
+output = getConcatenation(nums)
+print(output)
