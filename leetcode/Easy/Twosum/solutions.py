79d895e07458bf5bec3e18d09092efd145c38f95
AJackTi <dtrong97vn@gmail.com>
2023-04-07 22:58:47 +0700

    refactor: update problem Two Sum

--- leetcode/Easy/Twosum/solutions.py
+++
@@ -1,7 +0,0 @@
-def twoSum(nums, target):
-    dic = dict()
-    for index, num in enumerate(nums):
-        x = target - num
-        if x in dic:
-            return dic[x], index
-        dic[num] = index
