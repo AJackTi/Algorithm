2fa0a598d5aa27cfef9df90ca772c62195821aa2
AJackTi <dtrong97vn@gmail.com>
2023-04-08 10:18:24 +0700

    feat: 2011. Final Value of Variable After Performing Operations

---
+++ leetcode/Easy/2011. Final Value of Variable After Performing Operations/solutions.py
@@ -0,0 +1,8 @@
+def finalValueAfterOperations(operations: List[str]) -> int:
+    sum = 0
+    for i in operations:
+        if i == 'X++' or i == '++X':
+            sum += 1
+        else:
+            sum -= 1
+    return sum
