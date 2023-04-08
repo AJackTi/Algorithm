cc2baa0cb804bb89a1f611a80d8db734bb892e70
AJackTi <dtrong97vn@gmail.com>
2023-04-08 20:43:23 +0700

    feat: upgrade non_repeating to python3

--- string/non_repeating.py
+++ string/non_repeating.py
@@ -4,9 +4,9 @@ lst = list(str)
 # use set:
 for i in [(item, lst.count(item)) for item in set(lst)]:
     if i[1] == 1:
-		print i[0],
+        print(i[0])
 
 # use dictionary:
 for element in [{item: lst.count(item)} for item in lst]:
     if element.values() == [1]:
-        print element.keys(),
+        print (element.keys())
