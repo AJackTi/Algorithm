91311d46ee50b1448c39162b4f27e4eff3f23b49
AJackTi <dtrong97vn@gmail.com>
2023-04-09 22:14:29 +0700

    feat: add new solution to problem Divide Two Integers

--- leetcode/Medium/29. Divide Two Integers/solutions.py
+++ leetcode/Medium/29. Divide Two Integers/solutions.py
@@ -1,7 +1,40 @@
+# import math
+
+# def divide(dividend: int, divisor: int) -> int:
+#     result = math.trunc(dividend / divisor)
+#     if result >= 2147483648:
+#         return 2147483647
+#     return result
+
 import math
 
+MAX = 2147483648
+
 def divide(dividend: int, divisor: int) -> int:
-    result = math.trunc(dividend / divisor)
-    if result >= 2147483648:
-        return 2147483647
+    if dividend == -1 * MAX and divisor == 1 or dividend == MAX and divisor == - 1:
+            return dividend * divisor
+    flag = False
+    if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
+        flag = True
+    
+    dividend = abs(dividend)
+    divisor = abs(divisor)
+
+    if dividend == MAX and divisor == 1:
+        return MAX - 1 if not flag else (MAX - 1) * -1
+
+    result = 0
+    while dividend >= divisor:
+        shift = 0
+        while dividend >= divisor<<shift:
+            shift+=1
+
+        result += (1<<(shift-1))
+        dividend -= divisor<<(shift-1)
+    
+    if flag :
+        result *= -1
+    
     return result
+
+print(divide(-2147483648, -1))
