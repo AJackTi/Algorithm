2fdc3c1d0566707ec8f5b139857bcac4fb5cceb1
AJackTi <dtrong97vn@gmail.com>
2023-04-10 21:25:59 +0700

    feat: 14. Longest Common Prefix problem

--- leetcode/Easy/14. Longest Common Prefix/solutions.py
+++ leetcode/Easy/14. Longest Common Prefix/solutions.py
@@ -1,2 +1,11 @@
-def longestCommonPrefix(self, strs: List[str]) -> str:
-    return ""
+def longestCommonPrefix(strs: list[str]) -> str:
+    temp = min(word for word in strs)
+    i = 0
+    while i < len(temp):
+        for word in strs:
+            if word[i] != temp[i]:
+                return temp[0:i]
+        i+=1
+    return temp
+
+print(longestCommonPrefix(["a", "b"]))
