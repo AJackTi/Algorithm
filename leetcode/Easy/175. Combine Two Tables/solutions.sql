b53968597fa1a22a006c76f866373cbb71863d09
AJackTi <dtrong97vn@gmail.com>
2023-04-14 13:41:19 +0700

    175. Combine Two Tables

--- leetcode/Easy/175. Combine Two Tables/solutions.sql
+++ leetcode/Easy/175. Combine Two Tables/solutions.sql
@@ -1,2 +1,2 @@
-select firstName, lastName, city, state from Person
-left join Address on Person.personId = Address.personId
+SELECT firstName, lastName, city, state FROM Person
+LEFT JOIN Address ON Person.personId = Address.personId
