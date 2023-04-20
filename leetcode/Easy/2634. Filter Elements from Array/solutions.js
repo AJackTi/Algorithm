8e876e91652093baaf120a1298c097ca29474d4b
AJackTi <dtrong97vn@gmail.com>
2023-04-20 22:12:12 +0700

    feat: 2634. Filter Elements from Array problem

---
+++ leetcode/Easy/2634. Filter Elements from Array/solutions.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  let a = [];
+  let i;
+  for (i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      a.push(arr[i]);
+    }
+  }
+
+  return a;
+};
