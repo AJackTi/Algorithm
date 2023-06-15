ed7c49300bb734fce704a80fa9e52a76e0394e9d
AJackTi <dtrong97vn@gmail.com>
2023-06-15 11:05:01 +0700

    feat: 441. Arranging Coins problem

---
+++ leetcode/Easy/441. Arranging Coins/solutions.py
@@ -0,0 +1,20 @@
+def arrangeCoins(n: int) -> int:
+    left = 1
+    right = n
+    ans = 1
+
+    def calcCoinsRequired(r):
+        # 1 + 2 + 3 + ... + r
+        # r * (r + 1) / 2
+        return r * (r + 1) // 2
+
+    while left <= right:
+        mid = (left + right) // 2
+        nCoinsRequired = calcCoinsRequired(mid)
+        if n >= nCoinsRequired:
+            ans = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+    return ans
